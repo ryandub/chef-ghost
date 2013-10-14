4d1a1df7dc7dcd485a3fd9bbd1a1c69f03cedd38
Ryan Walker <ryan.walker@rackspace.com>
2013-10-14 17:16:04 -0500

    Bump version.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Rackspace'
 maintainer_email 'ryan.walker@rackspace.com'
 license          'Apache 2.0'
 description      'Installs/Configures Ghost CMS'
-version          '0.0.2'
+version          '0.0.3'
 
 %w{ ark database firewall git mysql nginx nodejs npm }.each do |cb|
   depends cb
